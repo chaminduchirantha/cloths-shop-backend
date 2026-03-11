f052200c3124227c69b7070420222818399b7391
chaminduchirantha <chaminduchirantha73@gmail.com>
2026-03-11 13:09:29 +0530

    update model and router

--- src/router/cloths.routes.ts
+++ src/router/cloths.routes.ts
@@ -6,7 +6,7 @@ import { UserRole } from "../model/userModel";
 import router from "./auth.routes";
 
 router.post("/createCloths",  authenticate , requireRole([UserRole.ADMIN]) , upload.single("image"),createCloths)
-router.get("/all" , upload.single("image") , getAll)
+router.get("/all", getAll)
 router.put("/updateCloths/:id", authenticate , requireRole([UserRole.ADMIN]) , upload.single("image"), updateCloths)
 router.get("/search", searchCloths)
 router.delete("/deleteCloths/:id", authenticate , requireRole([UserRole.ADMIN]) , deleteCloths)
