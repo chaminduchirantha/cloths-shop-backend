50433eadaa25d0fce877dd2ac2027761605d92e3
chaminduchirantha <chaminduchirantha73@gmail.com>
2026-03-15 10:25:16 +0530

    create the get all ordes , update status and create routes

--- src/controller/orders.controller.ts
+++ src/controller/orders.controller.ts
@@ -74,3 +74,64 @@ export const getAllOrders = async (req:Request , res:Response)=>{
         res.status(500).json({ message: "Server Error", error: error.message });
     }
 }
+
+
+export const updateOrderStatus = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const { status } = req.body;
+
+        if (!status) {
+            return res.status(400).json({ message: "Status is required" });
+        }
+
+        const allowedStatus = ["pending", "success", "cancelled"];
+        if (!allowedStatus.includes(status)) {
+            return res.status(400).json({ message: "Invalid status value" });
+        }
+
+        const updatedOrder = await Orders.findByIdAndUpdate(
+            id,
+            { status },
+            { new: true }
+        );
+
+        if (!updatedOrder) {
+            return res.status(404).json({ message: "Order not found" });
+        }
+
+        return res.status(200).json({
+            message: "Order status updated successfully",
+            data: updatedOrder
+        });
+
+    } catch (error: any) {
+        res.status(500).json({ message: "Server Error", error: error.message });
+    }
+};
+
+
+export const getOrdersByUser = async (req: Request, res: Response) => {
+    try {
+        const { email } = req.params;
+
+        if (!email) {
+            return res.status(400).json({ message: "Email is required" });
+        }
+
+        const orders = await Orders.find({ email }).sort({ createdAt: -1 });
+
+        if (!orders || orders.length === 0) {
+            return res.status(404).json({ message: "No orders found for this user" });
+        }
+
+        return res.status(200).json({
+            message: "User orders fetched successfully",
+            data: orders
+        });
+
+    } catch (error: any) {
+        return res.status(500).json({ message: "Server error", error: error.message });
+    }
+};
+
--- src/router/orders.routes.ts
+++ src/router/orders.routes.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { authenticate } from "../middleware/auth";
 import { requireRole } from "../middleware/role";
 import { UserRole } from "../model/userModel";
-import { saveOrder } from "../controller/orders.controller";
+import { getAllOrders, getOrdersByUser, saveOrder, updateOrderStatus } from "../controller/orders.controller";
 
 const router = Router();
 
 router.post("/saveOrders" , authenticate , requireRole([UserRole.USER]) , saveOrder)
-
+router.get("/allOrders" , authenticate ,requireRole([UserRole.ADMIN]), getAllOrders)
+router.put("/updateStatus/:id",authenticate,requireRole([UserRole.ADMIN]),updateOrderStatus);
+router.get("/viewOrder/:email", getOrdersByUser);
 
 export default router
